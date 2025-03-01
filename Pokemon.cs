6e09f04424ce712d928c53828ba362c125c0c762
Edoardo Tombolesi <edoardotombolesi8@gmail.com>
2025-03-01 03:50:56 -0300

    Classe Pokemon Feita

---
+++ Pokemon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeSharp
+{
+    class Pokemon
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public double altura { get; set; }
+
+        public double peso { get; set; }
+    }
+}
